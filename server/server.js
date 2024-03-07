d3db295653a33318a13abeadbd1b58a617fad48b
Kelly <83458976+kellyr27@users.noreply.github.com>
2024-03-08 10:14:13 +1100

    Adding CORS again

--- server/server.js
+++ server/server.js
@@ -6,11 +6,6 @@ const cors = require('cors');
 
 const {uploadAll} = require('./uploads/uploadDb');
 
-// controllers
-const routes = require('./routes/routes');
-const ascents = require('./routes/ascents');
-const stats = require('./routes/stats');
-
 // models
 const Route = require('./models/Route');
 const Ascent = require('./models/Ascent');
@@ -23,8 +18,37 @@ app.use(cors({
     origin: ['http://localhost:3000', 'https://indoor-climbing-log.onrender.com/'],
 }));
 
+// Set middleware of CORS 
+app.use((req, res, next) => {
+
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "https://indoor-climbing-log.onrender.com/"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+});
+
+
 // global variables
 const PORT = 5000;
+
+// controllers
+const routes = require('./routes/routes');
+const ascents = require('./routes/ascents');
+const stats = require('./routes/stats');
 
 // Routes
 // TODO: Fix this
